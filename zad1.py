3429dab3bd411703589614c5a8d3c5eac17525a2
Pawel-Rusak <122686290+Pawel-Rusak@users.noreply.github.com>
2023-11-28 20:32:32 +0100

    Update zad1.py

--- zad1.py
+++ zad1.py
@@ -6,38 +6,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def f(x):
-    return - 3*x**4 + 7*x**3 - 4*x + 6
+def f1(x):
+    return 1/3 * np.sin(3*x)
 
-def g(x):
-    return - 2*x**2 - 8*x + 1 + np.sin(x)
+def f2(x):
+    return 1/5 * np.sin(5*x)
 
-def u(x):
-    return np.exp(- x + 5)
-
-def v(x):
-    return np.sqrt(x + 3)
+def f3(x):
+    return 1/9 * np.sin(9*x)
 
 
-x = np.linspace(-8, 8, 200)
+
+
+x = np.linspace(-8, 8, 500)
 
 
 fig, ax = plt.subplots()
 
-ax.set_xlim(-8, 8)
-ax.set_ylim(-5, 10)
+ax.set_xlim(-1.3 , 1.3)
+ax.set_ylim(-0.4, 0.4)
 
 ax.grid()
 ax.set_xlabel("$x$")
 ax.set_ylabel("$y$")
-ax.set_title("Zadanie 1. Rysowanie wykresów funkcji.")
+ax.set_title("Paweł Rusak Rysowanie wykresów funkcji.")
 
-ax.plot(x, f(x), "-y", label = r"$y = - 3x^4 + 7x^3 - 4x + 6$")
-ax.plot(x, g(x), "--r", label = r"$y = - 2x^2 - 8x + 1 + \sin x$")
-ax.plot(x, u(x), ":b", label = r"$y = \mathrm{e}^{- x + 5}$")
-ax.plot(x, v(x), "-.g", label = r"$y = \sqrt{x + 3}$")
+ax.plot(x, f1(x), "-y", label = r"$y = \frac{1}{3}  \sin(3x)$")
+ax.plot(x, f2(x), "--r", label = r"$y = \frac{1}{5}  \sin(5x)$")
+ax.plot(x, f3(x), ":b", label = r"$y = \frac{1}{9}  \sin(9x)$")
 
 ax.legend()
 
 plt.show()
-fig.savefig("plots.pdf")
+
+
+ax.grid()
+ax.set_xlabel("$x$")
+ax.set_ylabel("$y$")
+ax.set_title("Paweł Rusak Zadanie 1. Rysowanie wykresów funkcji.")
+
+ax.plot(x, f1(x), "-y", label = r"$y = \frac{1}{3}  \sin(3x)$")
+ax.plot(x, f2(x), "--r", label = r"$y = \frac{1}{5}  \sin(5x)$")
+ax.plot(x, f3(x), ":b", label = r"$y = \frac{1}{9}  \sin(9x)$")
+
+fig.savefig("zad1.pdf")
